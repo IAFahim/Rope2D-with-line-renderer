9054982c7b1e064d972ad66955a0038ebe5fe86f
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2024-04-10 02:09:14 -0700

    Update Rope2D.cs

--- Rope2D.cs
+++ Rope2D.cs
@@ -8,9 +8,14 @@ public class Rope2D : MonoBehaviour
     public LineRenderer rope;
     public LayerMask collMask;
 
-    [field: SerializeReference] public List<Vector2> RopePositions { get; set; } = new();
-
-    private void Awake() => AddPosToRope(transform.position);
+    [field:SerializeReference] public List<Vector2> RopePositions { get; set; } = new();
+    private Vector3[] _ropePositionsV3Cache;
+    
+    private void Awake()
+    {
+        AddPosToRope(transform.position);
+        _ropePositionsV3Cache = new Vector3[RopePositions.Count];
+    }
 
     private void Update()
     {
@@ -23,46 +28,54 @@ public class Rope2D : MonoBehaviour
 
     private void DetectCollisionEnter()
     {
-        var position = player.position;
-        RaycastHit2D hit = Physics2D.Raycast(position,
-            (Vector2)rope.GetPosition(RopePositions.Count - 2) - (Vector2)position,
-            Vector2.Distance(position, rope.GetPosition(RopePositions.Count - 2)), collMask);
-        if (hit.collider != null)
+        RaycastHit2D hit = Physics2D.Linecast(player.position, rope.GetPosition(RopePositions.Count - 2), collMask);
+        if (hit)
         {
+            var hitPoint = hit.point;
+            if (hitPoint == RopePositions[^2]) return;
             RopePositions.RemoveAt(RopePositions.Count - 1);
-            AddPosToRope(hit.point);
+            AddPosToRope(hit.point);    
         }
     }
 
     private void DetectCollisionExits()
     {
-        var position = player.position;
-        RaycastHit2D hit = Physics2D.Raycast(position,
-            (Vector2)rope.GetPosition(RopePositions.Count - 3) - (Vector2)position,
-            Vector2.Distance(position, rope.GetPosition(RopePositions.Count - 3)), collMask);
-        if (hit.collider == null)
+        RaycastHit2D hit = Physics2D.Linecast(player.position, rope.GetPosition(RopePositions.Count - 3), collMask);
+        if (!hit)
         {
             RopePositions.RemoveAt(RopePositions.Count - 2);
         }
     }
 
-    private void AddPosToRope(Vector2 _pos)
+    private void AddPosToRope(Vector2 pos)
     {
-        RopePositions.Add(_pos);
+        RopePositions.Add(pos);
         RopePositions.Add(player.position); //Always the last pos must be the player
     }
 
+    
+
     private void UpdateRopePositions()
     {
+        if (_ropePositionsV3Cache.Length != RopePositions.Count)
+            _ropePositionsV3Cache = new Vector3[RopePositions.Count];
+
+        for (int i = 0; i < RopePositions.Count; i++)
+            _ropePositionsV3Cache[i] = RopePositions[i];
+
         rope.positionCount = RopePositions.Count;
-        var v3Array = new Vector3[RopePositions.Count];
-        var v2S = RopePositions.ToArray();
-        for (var i = 0; i < v2S.Length; i++)
+        rope.SetPositions(_ropePositionsV3Cache);
+    }
+
+    private Vector3[] ToVector3Array(Vector2[] v2Array)
+    {
+        Vector3[] v3 = new Vector3[v2Array.Length];
+        for (int i = 0; i < v2Array.Length; i++)
         {
-            v3Array[i] = v2S[i];
+            v3[i] = v2Array[i];
         }
 
-        rope.SetPositions(v3Array);
+        return v3;
     }
 
     private void LastSegmentGoToPlayerPos() => rope.SetPosition(rope.positionCount - 1, player.position);
